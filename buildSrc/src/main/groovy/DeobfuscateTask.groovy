512c4b18f1f4f3175917ec72fd985703988dbe6a
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-05-22 21:24:56 +0300

    Add proposed field names to deobf jar
    This required rewriting the JAR mapping to use
    Tiny Remapper instead of Enigma, since Enigma
    had some problems converting tinyv2 to
    Enigma mappings.

--- buildSrc/src/main/groovy/DeobfuscateTask.groovy
+++ buildSrc/src/main/groovy/DeobfuscateTask.groovy
@@ -1,13 +1,24 @@
-import cuchaz.enigma.command.DeobfuscateCommand
+import cuchaz.enigma.command.ConvertMappingsCommand
+import net.fabricmc.stitch.commands.tinyv2.CommandProposeV2FieldNames
+import net.fabricmc.tinyremapper.OutputConsumerPath
+import net.fabricmc.tinyremapper.TinyRemapper
+import net.fabricmc.tinyremapper.TinyUtils
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+
+import java.nio.file.Files
 
 class DeobfuscateTask extends DefaultTask {
     @InputDirectory
     File mappings
+
+    @InputFiles
+    FileCollection libraries
 
     @InputFile
     File inputJar
@@ -17,7 +28,35 @@ class DeobfuscateTask extends DefaultTask {
 
     @TaskAction
     def run() {
-        outputJar.parentFile.mkdirs()
-        new DeobfuscateCommand().run(inputJar.absolutePath, outputJar.absolutePath, mappings.absolutePath)
+        def inputTiny = Files.createTempFile("raw.", ".tiny")
+        def proposedTiny = Files.createTempFile("proposed.", ".tiny")
+        def converter = new ConvertMappingsCommand()
+
+        // Convert to tiny v2
+        converter.run("enigma", mappings.absolutePath, "tinyv2:official:named", inputTiny.toString())
+
+        // Propose names
+        new CommandProposeV2FieldNames().run([inputJar.absolutePath, inputTiny.toString(), proposedTiny.toString(), "false"] as String[])
+
+        def remapper = TinyRemapper.newRemapper()
+            .fixPackageAccess(true)
+            .withMappings(TinyUtils.createTinyMappingProvider(proposedTiny, "official", "named"))
+            .renameInvalidLocals(true)
+            .build()
+
+        def consumer = new OutputConsumerPath.Builder(outputJar.toPath()).build()
+        try {
+            consumer.addNonClassFiles(inputJar.toPath())
+
+            remapper.readInputs(inputJar.toPath())
+            for (def lib : libraries) {
+                remapper.readClassPath(lib.toPath())
+            }
+
+            remapper.apply(consumer)
+        } finally {
+            consumer.close()
+            remapper.finish()
+        }
     }
 }
